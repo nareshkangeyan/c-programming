7edf942b7a9aefe54a51f749fc424f18da63ad59
nareshkangeyan <123438370+nareshkangeyan@users.noreply.github.com>
2023-03-28 13:08:47 +0530

    Add files via upload

---
+++ area of the square.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter the number");
+    scanf("%d",&a);
+    printf("The area of the squre %d",a*a);
+	
+    
+}
---
+++ rectangle.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	
+	
+	printf("enter a value");
+	scanf("%d",&a);
+	printf("enter b value");
+	scanf("%d",&b);
+	
+	printf("The area of the rectangle is %d",a*b);
+	return 0;
+}
