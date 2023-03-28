f535499df8fdc8b33e86acb72262a83640331120
nareshkangeyan <123438370+nareshkangeyan@users.noreply.github.com>
2023-03-28 13:03:49 +0530

    Add files via upload

---
+++ natural number.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{ 
+int i,n,sum=0;
+printf("enter n value");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+
+
+sum=sum+i;
+}
+ printf("\n sum of n natural number %d",sum);
+ return 0;
+ }
