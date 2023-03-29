600fd686db53ecfba5574582d99d001c7d8040a6
nareshkangeyan <123438370+nareshkangeyan@users.noreply.github.com>
2023-03-29 09:15:51 +0530

    Add files via upload

---
+++ odd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,n;
+printf("enter the number");
+scanf("%d",&a);
+if(n%2==0)
+{
+printf("the given number is even");
+}
+else
+{
+printf("the given number is odd");
+}
+return 0;
+}
