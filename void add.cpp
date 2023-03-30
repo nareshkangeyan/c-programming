03213113fff9dfdf23c1c47d37c9ce0a6f5c61d9
nareshkangeyan <123438370+nareshkangeyan@users.noreply.github.com>
2023-03-30 10:32:30 +0530

    Add files via upload

---
+++ void add.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void add();       
+
+int main(){
+
+
+ add();            
+    return 0;
+
+}
+
+void add()
+{  int a,b,c;
+
+   scanf("%d",&a);  
+   scanf("%d",&b);
+
+   c=a+b;
+
+   printf("%d",c);
+
+}
